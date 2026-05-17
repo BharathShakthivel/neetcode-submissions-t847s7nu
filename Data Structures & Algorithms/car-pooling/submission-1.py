24324ae8063ff56ea1d3786ef41354775a7c99c1
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-05-17 23:44:14 +0100

    Add: car-pooling - submission-1

---
+++ Data Structures & Algorithms/car-pooling/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key = lambda t: t[1])
+        trip_heap =[] 
+        for trip in trips:
+            heapq.heappush(trip_heap,[trip[1],1,trip[0]])
+            heapq.heappush(trip_heap,[trip[2],0,trip[0]])
+        while trip_heap:
+            trip_point,cur_type,cur_capacity = heapq.heappop(trip_heap)
+            if cur_type == 0:
+                capacity+= cur_capacity
+            else:
+                capacity-= cur_capacity
+            if capacity<0:
+                return False
+        return True
